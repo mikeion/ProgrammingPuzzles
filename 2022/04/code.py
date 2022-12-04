87f959f7043f99d47d9829788918ad25331b2882
Mike Ion <mikeion@umich.edu>
2022-12-04 12:34:39 -0500

    push day 6
    push day 6 without print statements

--- 2022/04/code.py
+++ 2022/04/code.py
@@ -15,16 +15,12 @@ with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
         left_2, right_2 = int(assignment_2.group(1)), int(assignment_2.group(2))
         # part 1 - note this case is more restrictive and adds in the part 2 case
         if ((left_1 <= left_2) & (right_1 >= right_2)) or ((left_2 <= left_1) & (right_2 >= right_1)):
-            print(f'({left_1}, {right_1}) in ({left_2}, {right_2}) or ({left_2}, {right_2}) in ({left_1}, {right_1})')
             no_of_pairs +=1
             over_lapping +=1
         # part 2 - note it's overlapping if the largest of the left endpoints is 
         # less than smallest of the right endpoints.        
         elif max(left_1, left_2) <= min(right_1, right_2):
             over_lapping +=1
-        else:
-            print(f'({left_1}, {right_1}) in ({left_2}, {right_2}) or ({left_2}, {right_2}) in ({left_1}, {right_1})''doesn\'t work')
-
 print("Part One : "+ str(no_of_pairs))
 
 print("Part Two : "+ str(over_lapping))
