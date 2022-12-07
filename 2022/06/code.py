d08f7e9ed56a833d405c71be1086a54333158f97
Mike Ion <mikeion@umich.edu>
2022-12-07 09:42:11 -0500

    Push Day 6

--- 2022/06/code.py
+++ 2022/06/code.py
@@ -2,13 +2,26 @@
 # Author = Mike Ion
 # Date = December 2022
 
-with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
-    input = input_file.read()
+def open_file(code="code.py", file="input.txt"):
+    with open((__file__.rstrip(code)+file), 'r') as input_file:
+        f = input_file.read()
+        return f
 
+def part_1(code="code.py", file="input.txt"):
+    f = open_file(code, file)
+    for i in range(0, len(f)-4):
+        buffer = f[i:i+4]
+        buffer_set = set(buffer)
+        if len(buffer_set) == 4:
+            return i+4
+def part_2(code="code.py", file="input.txt"):
+    f = open_file(code, file)
+    for i in range(0, len(f)-14):
+        buffer = f[i:i+14]
+        buffer_set = set(buffer)
+        if len(buffer_set) == 14:
+            return i+14
 
+print("Part One : "+ str(part_1()))
 
-print("Part One : "+ str(None))
-
-
-
-print("Part Two : "+ str(None))
+print("Part Two : "+ str(part_2()))
